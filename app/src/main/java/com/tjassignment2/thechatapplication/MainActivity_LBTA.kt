fd8b1818a1090e989d9fc2a3d07c41754bb30d8a
tjfitzzster <tjfitzster@gmail.com>
2022-01-01 11:54:21 +0000

    Exploring different types of login and register pages.

---
+++ app/src/main/java/com/tjassignment2/thechatapplication/MainActivity_LBTA.kt
@@ -0,0 +1,11 @@
+package com.tjassignment2.thechatapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_LBTA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_lbta)
+    }
+}
